7b6888f0efa4e22a8d153d6b29d39887c86640ee
Sacha Meunier <sacha.meunier@student.hepl.be>
2026-02-18 11:09:34 +0100

    fix: the include paths
    - add const variables for paths
    - fix include paths for the layouts not working

--- views/404.php
+++ views/404.php
@@ -1,11 +1,11 @@
-<?php include_once 'partials/head.php' ?>
+<?php include VIEWS_DIR . '/partials/head.php' ?>
 <?php global $title ?>
 <main class="page-main">
     <h1><?php echo $title ?></h1>
 </main>
 
 
-<?php include_once 'partials/nav.php' ?>
+<?php include VIEWS_DIR . '/partials/nav.php' ?>
 
 
-<?php include_once 'partials/footer.php' ?>
+<?php include VIEWS_DIR . '/partials/footer.php' ?>
