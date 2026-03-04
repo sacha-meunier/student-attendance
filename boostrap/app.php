94deac2888468a170fba4bfaff9c38b5c9e86f5d
Sacha Meunier <sacha.meunier@student.hepl.be>
2026-03-04 09:01:44 +0100

    feat: create boostraping functionality

---
+++ boostrap/app.php
@@ -0,0 +1,7 @@
+<?php
+
+const ROOT_PATH = __DIR__.'/..';
+const PUBLIC_PATH = ROOT_PATH.'/public';
+const DB_PATH = ROOT_PATH.'/db';
+const VENDOR_PATH = ROOT_PATH.'/vendor';
+const VIEWS_PATH = ROOT_PATH.'/views';
