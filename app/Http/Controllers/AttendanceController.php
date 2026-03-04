eeeff3663f0a628266bca1eb35f7e7cb7d7ae190
Sacha Meunier <sacha.meunier@student.hepl.be>
2026-03-04 10:57:49 +0100

    fix: attendances requests

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -2,6 +2,10 @@
 
 function index()
 {
+    require MODELS_PATH.'/Attendance.php';
+
     $title = 'Prendre les présences';
+    $students = all();
+
     include VIEWS_PATH.'/attendances/index.php';
 }
--- views/attendances/index.php
+++ views/attendances/index.php
@@ -1,13 +1,14 @@
 <?php include VIEWS_PATH.'/partials/head.php' ?>
+
 <main class="page-main">
-    <h1><?php echo $title ?></h1>
+    <h1><?= $title ?></h1>
     <form action="" method="POST">
         <ol class="student-list">
-            <?php foreach (getAllStudents() as $student) { ?>
+            <?php foreach ($students as $student) { ?>
                 <li>
-                    <input id="<?php echo $student['id'] ?>" type="checkbox" name="students[]"
-                           value="<?php echo $student['id'] ?>">
-                    <label for="<?php echo $student['id'] ?>"><?php echo $student['first_name'] ?>
+                    <input id="<?= $student['id'] ?>" type="checkbox" name="students[]"
+                           value="<?= $student['id'] ?>">
+                    <label for="<?= $student['id'] ?>"><?= $student['first_name'] ?>
                         &nbsp;<?php echo $student['last_name'] ?></label>
                 </li>
             <?php } ?>
@@ -18,7 +19,6 @@
     <button class="randomStudentBtn hidden">Choisir un·e étudiant·e</button>
     <p class="currentStudent hidden"></p>
 </main>
-
 
 <?php include VIEWS_PATH.'/partials/nav.php' ?>
 
