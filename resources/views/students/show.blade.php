b2acff336d86bda67be74002bb6018800b477611
Sacha Meunier <sacha.meunier@student.hepl.be>
2026-04-13 23:19:12 +0200

    design: improve create and add back button component

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -96,12 +96,5 @@
         </div>
     </div>
 
-    <a href="{{ route('students.index') }}"
-       class="inline-flex items-center px-6 py-3 bg-gray-200 text-gray-900 font-medium rounded-lg hover:bg-gray-300 transition-colors">
-        <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
-        </svg>
-        Voir tous les étudiants
-    </a>
+    <x-button-back route="{{ route('students.index') }}" label="Voir tous les étudiants"/>
 </x-layout>
